5d9e09d3b50600a99035a6a145e9b5b7b53caac3
Mkalo <matt@mkalo.dev>
2023-09-21 02:54:28 -0300

    Error in zod transform

--- api/index.ts
+++ api/index.ts
@@ -12,14 +12,24 @@ export const app = new Hono({ strict: false }).basePath('/api');
 app.use('*', cors());
 
 const dateParamsSchema = z.object({
-  date: z.string().optional().transform((v) => v ? isNaN(Number(v)) ? new Date(v) : new Date(Number(v)) : new Date()),
+  date: z.string().optional().transform((v, ctx) => {
+    const timestamp = Number(v);
+    const date = v ? new Date(isNaN(timestamp) ? v : timestamp) : new Date();
+
+    if (isNaN(date.getTime())) {
+      ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Invalid Date" });
+      return z.NEVER;
+    }
+
+    return date;
+  }),
 });
 
 app.get('/:date?',
   (c) => {
     const res = dateParamsSchema.safeParse(c.req.param());
-    if (!res.success || isNaN(res.data.date.getTime())) {
-      return c.json({ error: "Invalid Date" });
+    if (!res.success) {
+      return c.json({ error: res.error.message });
     }
 
     const date = res.data.date ?? new Date();
