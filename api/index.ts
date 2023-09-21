0d526b5dd457ed83ba146d2601fc0663a47e9773
Mkalo <matt@mkalo.dev>
2023-09-21 01:53:20 -0300

    Add zod validator

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,23 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
 
 export const config = {
   runtime: 'edge',
-}
+};
 
-const app = new Hono().basePath('/api')
+export const app = new Hono().basePath('/api');
 
-app.get('/', (c) => c.json({ message: 'Hello Hono!' }))
+app.get('/', (c) => c.json({ message: 'Hello Hono!' }));
 
-export default handle(app)
+app.get('/hello/:name',
+  zValidator('param', z.object({
+    name: z.string().min(3),
+  })),
+  (c) => {
+    return c.json({ message: `Hello ${c.req.param.name}!` })
+  }
+);
+
+export default handle(app);
